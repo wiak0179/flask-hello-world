f001b11467fc5329f70a2c211718a2b67cabf56e
Cole Akers <wiak0179@colorado.edu>
2025-04-01 19:20:17 +0000

    add db_create route to make basketball table

--- app.py
+++ app.py
@@ -10,5 +10,19 @@ def hello_world():
 @app.route('/db_test')
 def tessting():
     conn = psycopg2.connect("postgresql://akers_flask_lab10_db_user:q2hbLB5oMeWlBSmKZkQGYv1AfeGLg0QH@dpg-cvm2ffggjchc73evir7g-a/akers_flask_lab10_db")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+    
+    
+    
+    conn.commit()
     conn.close()
-    return "Database connection Successful"
+    return "Basketball Table Successfully Created"
