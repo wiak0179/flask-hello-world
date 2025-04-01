418fbe536657bcb7d6f8e1b425b86dcd3260bb62
Cole Akers <wiak0179@colorado.edu>
2025-04-01 19:27:07 +0000

    db_create running correctly now

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ def hello_world():
 
 
 @app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("postgresql://akers_flask_lab10_db_user:q2hbLB5oMeWlBSmKZkQGYv1AfeGLg0QH@dpg-cvm2ffggjchc73evir7g-a.oregon-postgres.render.com/akers_flask_lab10_db")
+    conn.close()
+    return "Successfully connected to the database!"
+
+
+
+@app.route('/db_create')
 def tessting():
     conn = psycopg2.connect("postgresql://akers_flask_lab10_db_user:q2hbLB5oMeWlBSmKZkQGYv1AfeGLg0QH@dpg-cvm2ffggjchc73evir7g-a/akers_flask_lab10_db")
     cur = conn.cursor()
