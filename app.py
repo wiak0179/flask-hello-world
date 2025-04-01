a15137aa1ec289e68414a7de16b2f4f89ffd6bd9
Cole Akers <wiak0179@colorado.edu>
2025-04-01 19:34:36 +0000

    db_insert added

--- app.py
+++ app.py
@@ -27,10 +27,25 @@ def tessting():
         Name varchar(255),
         Number int
         );
-    ''')
-    
-    
-    
+    ''') 
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Created"
+
+
+
+@app.route('/db_insert')
+def db_insert():
+    conn = psycopg2.connect("postgresql://akers_flask_lab10_db_user:q2hbLB5oMeWlBSmKZkQGYv1AfeGLg0QH@dpg-cvm2ffggjchc73evir7g-a.oregon-postgres.render.com/akers_flask_lab10_db")
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+         ''')
+    conn.commit()
+    conn.close()
+    return "Inserted data into Basketball table!"
