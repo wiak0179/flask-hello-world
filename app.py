b1e291e1b3bc54b59d157caf3c028ea17016ab25
Cole Akers <wiak0179@colorado.edu>
2025-04-01 18:18:10 +0000

    route to SQL database added to app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import psycopg2
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Cole Akers in 3308'
+
+
+@app.route('/db_test')
+def tessting():
+    conn = psycopg2.connect("your_db_url_here")
+    conn.close()
+    return "Database connection Successful"
